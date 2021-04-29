048470494998495e7b09fb5a0a1f2ba3d893cfa5
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2021-04-29 05:11:22 -0400

    more tab experiments

--- tab.py
+++ tab.py
@@ -6,7 +6,7 @@ frets={notes[k]:k for k in notes}
 
 Note = namedtuple('note',['string','fret','duration'])
 
-Tuning = {0:'d',1:'c',2:'g',3:'c',4:'g'}
+Tuning = {0:('d',1),1:('c',1),2:('g',0),3:('c',0),4:('g',1)}
 
 def fret_to_notes(base, fret):
     start_fret = frets[base]
@@ -18,16 +18,82 @@ def fret_to_notes(base, fret):
 
 def decode(s):
     loc, fret, dur = s.split('.')
-    lily = fret_to_notes(Tuning[int(loc)],int(fret))+dur
+    tuning_note = Tuning[int(loc)][0]
+    tuning_octave=Tuning[int(loc)][1]
+    octave_mark = '' if tuning_octave==0 else '\''
+    lily = fret_to_notes(tuning_note,int(fret))+octave_mark+dur
     return lily
 
 
-test = "2.0.4 0.2.8 0.0.8 0.2.8 0.3.8 0.2.8 0.0.8"
+Apart1 = """
+2.0.4 0.2.8 0.0.8 0.2.8 0.3.8 0.2.8 0.0.8
+1.0.8 0.2.8 0.0.8 1.0.8 2.2.8 2.4.8 1.0.8 2.2.8
+2.0.4 1.0. 0.2.4 1.0.4
+0.0.4 2.0.4 0.0.2
+2.0.4 0.2.8 0.0.8 0.2.8 0.3.8 0.2.8 0.0.8
+1.0.8 0.2.8 0.0.8 1.0.8 2.2.8 2.4.8 1.0.8 2.2.8
+2.0.4 3.4.4 3.5.8 3.4.8 3.2.4
+""".split()
+
+Apart_first_ending="3.0.4 1.0.4 3.0.2".split()
+
+Apart_second_ending= "3.0.4 1.0.4 3.0.4 1.0.8 2.2.8".split()
+
+Bpart1 = """
+2.0.4 3.4.4 3.4.4 1.0.8 2.2.8
+2.0.4 3.4.4 3.4.4 1.0.8 2.2.8
+2.0.4 3.4.4 3.5.4 3.4.4
+3.2.8 3.0.8 3.2.8 3.4.8 3.2.4 1.0.8 2.2.8
+2.0.8 3.4.8 3.0.8 3.4.8 2.0.4 3.4.4
+3.5.4 2.0.4 2.2.8 2.4.8 1.0.8 2.2.8
+2.0.4 1.0.4 0.2.4 0.0.4
+""".split()
+
+Bpart_first_ending="1.0.4 2.0.4 1.0.4 1.0.8 2.2.8".split()
+
+Bpart_second_ending="1.0.4 2.0.4 1.0.2".split()
+
+
+
 
 print("\\version \"2.22.1\"")
+print("\\paper { indent=0 }")
+print("\\header {title=\"New Five Cent\"}")
 print("{")
-measure = test.split()
-for x in measure:
+print("\\time 4/4")
+print("\\repeat volta 2 {")
+for x in Apart1:
     lily = decode(x)
     print(lily) 
+print("}")
+print("\\alternative {")
+print("{")
+for x in Apart_first_ending:
+    lily=decode(x)
+    print(lily)
+print("}")
+print("{")
+for x in Apart_second_ending:
+    lily=decode(x)
+    print(lily)
+print("}")
+print("}")
+print("\\repeat volta 2 {")
+for x in Bpart1:
+    lily = decode(x)
+    print(lily) 
+print("}")
+print("\\alternative {")
+print("{")
+for x in Bpart_first_ending:
+    lily=decode(x)
+    print(lily)
+print("}")
+print("{")
+for x in Bpart_second_ending:
+    lily=decode(x)
+    print(lily)
+print("}")
+print("}")
+print("\\bar \"|.\"")
 print("}")
--- test.ly
+++ test.ly
@@ -1,12 +1,124 @@
 \version "2.22.1"
-{
-\relative{
+\header {title="New Five Cent" composer="Traditional"}
+\paper {indent=0}
+music = {
+\time 4/4
+\key c \major
+\repeat volta 2 {
 g'4
-e'8
-d'8
-e'8
+e''8
+d''8
+e''8
+f''8
+e''8
+d''8
+c''8
+e''8
+d''8
+c''8
+a'8
+b'8
+c''8
+a'8
+g'4
+c''
+e''4
+c''4
+d''4
+g'4
+d''2
+g'4
+e''8
+d''8
+e''8
+f''8
+e''8
+d''8
+c''8
+e''8
+d''8
+c''8
+a'8
+b'8
+c''8
+a'8
+g'4
+e'4
 f'8
 e'8
+d'4
+}
+\alternative {
+{
+c'4
+c''4
+c'2
+}
+{
+c'4
+c''4
+c'4
+c''8
+a'8
+}
+}
+\repeat volta 2 {
+g'4
+e'4
+e'4
+c''8
+a'8
+g'4
+e'4
+e'4
+c''8
+a'8
+g'4
+e'4
+f'4
+e'4
 d'8
+c'8
+d'8
+e'8
+d'4
+c''8
+a'8
+g'8
+e'8
+c'8
+e'8
+g'4
+e'4
+f'4
+g'4
+a'8
+b'8
+c''8
+a'8
+g'4
+c''4
+e''4
+d''4
+}
+\alternative {
+{
+c''4
+g'4
+c''4
+c''8
+a'8
+}
+{
+c''4
+g'4
+c''2
 }
 }
+\bar "|."
+}
+
+\score {
+  \new Staff {
+    \transpose c d {\music }
+}}
